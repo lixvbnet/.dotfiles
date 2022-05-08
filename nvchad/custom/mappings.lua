bec6e62aaa8ce0a3b0a8be5a0cb06fb404d05143
lixvbnet <lixvbnet@gmail.com>
2022-05-08 23:53:30 +0800

    bind Alt-Up/Down : move current line

--- nvchad/custom/mappings.lua
+++ nvchad/custom/mappings.lua
@@ -1,1 +1,7 @@
--- local map = require("core.utils").map
+local map = require("core.utils").map
+
+-- map("n", "<leader>q", "<cmd> :q <CR>")
+
+-- Move current line/block up/down with Alt-Up/Down
+map("n", "<A-Up>", "<Esc>:m .-2<CR>")
+map("n", "<A-Down>", "<Esc>:m .+1<CR>")
