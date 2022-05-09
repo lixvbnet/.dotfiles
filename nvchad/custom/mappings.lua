61ba37b79effe39dbd2039c5d99318444f9973bc
lixvbnet <lixvbnet@gmail.com>
2022-05-09 19:55:14 +0800

    fix Alt-Up/Down

--- nvchad/custom/mappings.lua
+++ nvchad/custom/mappings.lua
@@ -3,5 +3,10 @@ local map = require("core.utils").map
 -- map("n", "<leader>q", "<cmd> :q <CR>")
 
 -- Move current line/block up/down with Alt-Up/Down
-map("n", "<A-Up>", "<Esc>:m .-2<CR>")
-map("n", "<A-Down>", "<Esc>:m .+1<CR>")
+map("i", "<A-Up>", "<Esc>:m .-2<CR>==gi")
+map("i", "<A-Down>", "<Esc>:m .+1<CR>==gi")
+
+map("n", "<A-Up>", ":m .-2<CR>==")
+map("n", "<A-Down>", ":m .+1<CR>==")
+
+
