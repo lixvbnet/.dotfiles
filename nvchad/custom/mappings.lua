9ae3c0cabcfb5e96a559b8493f6d7ebe9d1a86cc
lixvbnet <lixvbnet@gmail.com>
2022-05-10 21:15:29 +0800

    use A-w to switch between windows

--- nvchad/custom/mappings.lua
+++ nvchad/custom/mappings.lua
@@ -9,6 +9,11 @@ map("t", "<C-q>", "<C-\\><C-n>")
 -- Ctrl + s to save file
 map("i", "<C-s>", "<Esc>:w<CR>")
 
+-- Alt + w to switch between windows
+map("n", "<A-w>", "<C-w>w")
+map("i", "<A-w>", "<Esc> <C-w>w")
+map("t", "<A-w>", "<C-\\><C-n> <C-w>w")
+
 -- Move current line/block up/down with Alt-Up/Down
 map("i", "<A-Up>", "<Esc>:m .-2<CR>==gi")
 map("i", "<A-Down>", "<Esc>:m .+1<CR>==gi")
