5dec730be0e2cd305109905bbfaa5dccd3148254
lixvbnet <lixvbnet@gmail.com>
2022-05-10 18:55:05 +0800

    Ctrl + q to exit insertion/terminal mode

--- nvchad/custom/mappings.lua
+++ nvchad/custom/mappings.lua
@@ -2,8 +2,9 @@ local map = require("core.utils").map
 
 -- map("n", "<leader>q", "<cmd> :q <CR>")
 
--- Ctrl + q to exit insertion mode
+-- Ctrl + q to exit insertion/terminal mode
 map("i", "<C-q>", "<Esc>")
+map("t", "<C-q>", "<C-\\><C-n>")
 
 -- Ctrl + s to save file
 map("i", "<C-s>", "<Esc>:w<CR>")
@@ -14,5 +15,3 @@ map("i", "<A-Down>", "<Esc>:m .+1<CR>==gi")
 
 map("n", "<A-Up>", ":m .-2<CR>==")
 map("n", "<A-Down>", ":m .+1<CR>==")
-
-
