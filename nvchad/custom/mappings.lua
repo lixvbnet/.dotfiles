7b3bcab09249224459affb85144623779db2f27d
lixvbnet <lixvbnet@gmail.com>
2022-05-10 15:31:58 +0800

    C-q to exit insertion mode, C-s to save file

--- nvchad/custom/mappings.lua
+++ nvchad/custom/mappings.lua
@@ -2,6 +2,12 @@ local map = require("core.utils").map
 
 -- map("n", "<leader>q", "<cmd> :q <CR>")
 
+-- Ctrl + q to exit insertion mode
+map("i", "<C-q>", "<Esc>")
+
+-- Ctrl + s to save file
+map("i", "<C-s>", "<Esc>:w<CR>")
+
 -- Move current line/block up/down with Alt-Up/Down
 map("i", "<A-Up>", "<Esc>:m .-2<CR>==gi")
 map("i", "<A-Down>", "<Esc>:m .+1<CR>==gi")
