31b23fbc439b913e246b0cedc7a330a142c979b4
lixvbnet <lixvbnet@gmail.com>
2022-05-14 16:31:27 +0800

    replace close_buffer function (leader +x)

--- nvchad/custom/init.lua
+++ nvchad/custom/init.lua
@@ -1,1 +1,14 @@
 require "custom.mappings"
+
+
+-- replace close_buffer function (leader + x)
+nvchad.close_buffer = function(force)
+    if vim.bo.buftype == "terminal" then
+       vim.api.nvim_win_hide(0)
+       return
+    end
+
+    force = force or not vim.bo.buflisted or vim.bo.buftype == "nofile"
+    local close_cmd = force and ":bd!" or ":bd"
+    vim.cmd(close_cmd)
+ end
