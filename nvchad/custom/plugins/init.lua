b714659f7d70a8209abe27ca32b61f01384b1920
lixvbnet <lixvbnet@gmail.com>
2022-05-15 12:20:47 +0800

    base46 use history version

--- nvchad/custom/plugins/init.lua
+++ nvchad/custom/plugins/init.lua
@@ -23,10 +23,24 @@ return {
     --     end,
     --  },
 
+    ["NvChad/base46"] = {
+       commit = "2e925ef",      -- use a specified version (git commit)
+       after = "packer.nvim",
+       config = function()
+          local ok, base46 = pcall(require, "base46")
+
+          if ok then
+             base46.load_theme()
+          end
+      end,
+    },
+
+
     ["NvChad/nvterm"] = {
         commit = "4efafd5",     -- use a specified version (git commit)
         config = function()
            require "plugins.configs.nvterm"
         end,
-     },
+    },
+    
 }
