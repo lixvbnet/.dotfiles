94b0983816c17a8244f4983cb3fb04a3eae9b7b3
lixvbnet <lixvbnet@gmail.com>
2025-12-07 22:50:21 +0800

    ignore error if python3 or certifi is not installed

--- .bashrc.darwin.sh
+++ .bashrc.darwin.sh
@@ -5,7 +5,7 @@ alias ls='ls -G'
 export PATH="$PATH:/Applications/010 Editor.app/Contents/CmdLine"
 
 # fix pipenv SSL cert issue in MacOS
-command -v python3 &> /dev/null && export REQUESTS_CA_BUNDLE=$(python3 -m certifi) || true
+command -v python3 &> /dev/null && export REQUESTS_CA_BUNDLE=$(python3 -m certifi) &> /dev/null || true
 
 
 ################# TMUX (SHOULD BE PUT AT LAST) #################
