437b11027e276c62a8e73af99cca94400eab70c2
lixvbnet <lixvbnet@gmail.com>
2026-01-05 15:18:30 +0800

    update .bashrc.windows.sh

--- .bashrc.windows.sh
+++ .bashrc.windows.sh
@@ -1,5 +1,8 @@
 ## This file contains OS spefic settings for Windows
 
+alias lab='jupyter lab -y --notebook-dir=/e/Documents/jupyter/'
+
+
 # If in IDEA, also export WT_SESSION so that some programs consider it Windows Terminal
 if [ "$TERMINAL_EMULATOR" = "JetBrains-JediTerm" ]; then
     export WT_SESSION=1
