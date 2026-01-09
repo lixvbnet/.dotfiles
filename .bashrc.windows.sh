31f920e4302dcf46f458c0ea61993a60398d6279
lixvbnet <lixvbnet@gmail.com>
2026-01-09 18:30:39 +0800

    alias for uvsh and set UV_CACHE_DIR in Windows

--- .bashrc.windows.sh
+++ .bashrc.windows.sh
@@ -1,7 +1,8 @@
 ## This file contains OS spefic settings for Windows
 
 alias lab='jupyter lab -y --notebook-dir=/e/Documents/jupyter/'
-
+alias uvsh='source .venv/Scripts/activate'
+export UV_CACHE_DIR='/e/Temp/uv-cache'
 
 # If in IDEA, also export WT_SESSION so that some programs consider it Windows Terminal
 if [ "$TERMINAL_EMULATOR" = "JetBrains-JediTerm" ]; then
