914d6e13d3ad817996276d31ce7fab3b2d021c62
lixvbnet <lixvbnet@gmail.com>
2026-04-24 20:06:08 +0800

    use awslocal alias in Windows

--- .bashrc.windows.sh
+++ .bashrc.windows.sh
@@ -4,6 +4,9 @@ alias lab='jupyter lab -y --notebook-dir=/e/Documents/jupyter/'
 alias uvsh='source .venv/Scripts/activate'
 export UV_CACHE_DIR='/e/Temp/uv-cache'
 
+# The awslocal python script has path issues in Windows. Simply use the alias (without DEFAULT_REGION env) instead.
+alias awslocal="AWS_ACCESS_KEY_ID=test AWS_SECRET_ACCESS_KEY=test aws --endpoint-url=http://${LOCALSTACK_HOST:-localhost}:4566"
+
 # If in IDEA, also export WT_SESSION so that some programs consider it Windows Terminal
 if [ "$TERMINAL_EMULATOR" = "JetBrains-JediTerm" ]; then
     export WT_SESSION=1
