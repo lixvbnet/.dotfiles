fd7979e34630825adef51eecc131e4a3300fa0b7
lixvbnet <lixvbnet@gmail.com>
2026-04-26 10:43:52 +0800

    use awslocal alias in Windows - pointing to LocalStack in the VM

--- .bashrc.windows.sh
+++ .bashrc.windows.sh
@@ -5,6 +5,8 @@ alias uvsh='source .venv/Scripts/activate'
 export UV_CACHE_DIR='/e/Temp/uv-cache'
 
 # The awslocal python script has path issues in Windows. Simply use the alias (without DEFAULT_REGION env) instead.
+# (and pointing to LocalStack in the VM. Because Docker for Windows, which uses WSL2, makes the OS quite unstable.)
+export LOCALSTACK_HOST=192.168.10.3
 alias awslocal="AWS_ACCESS_KEY_ID=test AWS_SECRET_ACCESS_KEY=test aws --endpoint-url=http://${LOCALSTACK_HOST:-localhost}:4566"
 
 # If in IDEA, also export WT_SESSION so that some programs consider it Windows Terminal
